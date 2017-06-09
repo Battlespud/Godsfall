b746c698b19134baa89c5b7c77b4f1c31f885c90
xnenaixyinaa <rxjohnston@gmail.com>
2017-06-09 14:41:39 -0800

    yoloyoloducksauce

---
+++ Assets/Main/Scenes/Bridge Scene/GoToShop.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToShop : MonoBehaviour, IEventInitializer
+{
+
+    public int lockoutTimer = 3; //so we dont instantly trigger the teleport when we touch appear on top of the other trigger
+
+    public bool useLayersMethod = false;
+
+    public const float yShopLayer = 100f;
+
+    public GoToShop targetTrigger;
+    public Vector3 targetLoc;
+
+    //ours
+    public Vector3 Loc;
+
+    void Start()
+    {
+        triggerLoc = gameObject.transform.position;
+        targetLoc = targetTrigger.Loc;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    void OnTriggerEnter(Collision col)
+    {
+        Debug.Log("Triggered reeeee");
+        switch (useLayersMethod)
+        {
+            case true:
+                moveToShopLayers(col);
+                break;
+
+            case false:
+                moveToShopDefault(col);
+                break;
+        }
+    }
+
+
+    private void moveToShopLayers(Collision col)
+    {
+        try
+        {
+            col.gameObject.GetComponent<MovementController>().teleport(new Vector3(triggerLoc.x, yShopLayer, triggerLoc.z);
+        }
+        catch
+        {
+            Debug.Log("Invalid object triggering");
+        }
+    }
+
+    private void moveToShopDefault(Collision col)
+    {
+        try
+        {
+            targetTrigger.gameObject.SetActive(false);
+            col.gameObject.GetComponent<MovementController>().teleport(targetLoc);
+            Invoke("EnableOther", lockoutTimer);
+        }
+        catch
+        {
+            Debug.Log("Invalid object triggering");
+        }
+    }
+    void EnableOther()
+    {
+        targetTrigger.gameObject.SetActive(true);
+    }
+
+    public void initializeEvents()
+    {
+
+    }
+    void teleport()
+    {
+
+    }
+
+}
