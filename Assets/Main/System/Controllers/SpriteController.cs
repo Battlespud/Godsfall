d4d2fa535f6c15a2a51d0bd9f70870091865ee64
Battlespud <battlespud@hotmail.com>
2017-06-10 23:25:31 -0400

    at least it still doesnt glitch out tho

--- Assets/Main/System/Controllers/SpriteController.cs
+++ Assets/Main/System/Controllers/SpriteController.cs
@@ -116,7 +116,7 @@ public class SpriteController : MonoBehaviour{
 		int finalfinalfacing = (int)finalFacing - (int)cameraDirection.facing;
 		if (finalfinalfacing  < 0)
 			finalfinalfacing += 8; //wrap around
-		sr.sprite = bodySpritesArray [finalfinalfacing];
+		sr.sprite = bodySpritesArray [(int)finalFacing];
 
 	}
 
