1eff9f57d02b8dd27107861633a878f9a642d9ed
Battlespud <battlespud@hotmail.com>
2017-06-13 20:22:04 -0400

    Bigger changes: Dialog
    All dialog should go into the dialogue xml in resources folder.  It will be autoloaded by the dictionary (as long as your scene has an initializer script somewhere in it) and can be retrieved as strings via the RetrieveSpeech(SpeechKey) method. SpeechKey is just 2 ints

---
+++ Assets/Main/System/Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initializer : MonoBehaviour {
+
+
+	//use this to initialize static classes etc
+
+
+	// Use this for initialization
+	void Start () {
+		SpeechRepo.InitializeDictionary ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
