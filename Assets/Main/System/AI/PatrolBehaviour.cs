f2b258396faa3d5adc97cdf76f4d026057ff5dc7
Battlespud <battlespud@hotmail.com>
2017-06-15 22:01:48 -0400

    Added guidance system to patrol
    NPCS will now automatically course correct if pushed.  wont work for massive shoves right at the end of a patrol, but realistically that cant happen in any way ingame.

--- Assets/Main/System/AI/PatrolBehaviour.cs
+++ Assets/Main/System/AI/PatrolBehaviour.cs
@@ -27,6 +27,16 @@ public class PatrolBehaviour : MonoBehaviour , IEventInitializer {
 
 	private const string endpointTag = ("PatrolEndpoint");
 
+	private Vector3 guidanceVec;
+	private Vector3 correctionVec;
+	public bool guidanceActive = false;
+	public float guideScale = .5f;
+	public float guideStep = .25f;
+
+	[SerializeField]bool xBeingCorrected = false;
+	[SerializeField]bool yBeingCorrected = false;
+	[SerializeField]bool zBeingCorrected = false;
+
 	//axis to travel along
 
 	public int heading = 1;
@@ -44,6 +54,7 @@ public class PatrolBehaviour : MonoBehaviour , IEventInitializer {
 	public void DirectSet(int head, Axis ax){
 		heading = head;
 		axis = ax;
+		updateGuidance ();
 	}
 
 
@@ -56,31 +67,116 @@ public class PatrolBehaviour : MonoBehaviour , IEventInitializer {
 		movementController = this.gameObject.GetComponent<MovementController> ();
 		initializeEvents ();
 		go = this.gameObject;
+		guidanceVec = new Vector3 (0f,0f,0f);
+		updateGuidance ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (!paused) {
+			Vector3 toMove = new Vector3 ();
 			switch (axis) {
 			case Axis.X:
-				movementController.npcInputToMove (new Vector3 (heading * Time.fixedDeltaTime * speedMultiplier, 0, 0));
+				toMove = new Vector3 (heading * Time.fixedDeltaTime * speedMultiplier, 0, 0);
 				break;
 			case Axis.Y:
-				movementController.npcInputToMove (new Vector3 (0, heading * Time.fixedDeltaTime * speedMultiplier, 0));
+				toMove = new Vector3 (0, heading * Time.fixedDeltaTime * speedMultiplier, 0);
 				break;
 			case Axis.Z:
-				movementController.npcInputToMove (new Vector3 (0, 0, heading * Time.fixedDeltaTime * speedMultiplier));
+				toMove = new Vector3 (0, 0, heading * Time.fixedDeltaTime * speedMultiplier);
+				break;
+
+			}
+			courseCorrection ();
+			toMove += correctionVec;
+			Debug.Log (correctionVec);
+			movementController.npcInputToMove (toMove);
+
+		}
+	}
+
+	void courseCorrection(){
+		xBeingCorrected = false;yBeingCorrected = false;zBeingCorrected = false;
+		correctionVec = new Vector3 (0f,0f,0f);
+		if (guidanceActive) {
+			switch (axis) {
+			case Axis.X:
+				if (Mathf.Abs(transform.position.z - guidanceVec.z) > guideScale) {
+					guidanceFixZ ();
+				}
+				if (Mathf.Abs(transform.position.y - guidanceVec.y) > guideScale) {
+					guidanceFixY ();
+				}
+				break;
+
+			case Axis.Y:
+				if (Mathf.Abs(transform.position.x - guidanceVec.x) > guideScale) {
+					guidanceFixX ();
+				}
+				if (Mathf.Abs(transform.position.z - guidanceVec.z) > guideScale) {
+					guidanceFixZ ();
+				}
+				break;
+
+			case Axis.Z:
+				if (Mathf.Abs(transform.position.x - guidanceVec.x) > guideScale) {
+					guidanceFixX ();
+				}
+				if (Mathf.Abs(transform.position.y - guidanceVec.y) > guideScale) {
+					guidanceFixY ();
+				}
 				break;
 
 			}
 		}
 	}
 
+	void guidanceFixX(){
+		//too +x
+		if (transform.position.x > guidanceVec.x) {
+			correctionVec.x -= guideStep;
+		}
+		//too -x
+		if (transform.position.x < guidanceVec.x) {
+			correctionVec.x += guideStep;
+		}
+		xBeingCorrected = true;
+	}
+
+	void guidanceFixY(){
+		//too +y
+		if (transform.position.y > guidanceVec.y) {
+		//	correctionVec.y -= guideStep;
+		}
+		//too -y
+		if (transform.position.y < guidanceVec.y) {
+		//	correctionVec.y += guideStep;
+		}
+		yBeingCorrected = true;
+	}
+
+	void guidanceFixZ(){
+		//too +z
+		if (transform.position.z > guidanceVec.z) {
+			correctionVec.z -= guideStep;
+		}
+		//too -z
+		if (transform.position.z < guidanceVec.z) {
+			correctionVec.z -= guideStep;
+		}
+		zBeingCorrected = true;
+	}
+
+	void updateGuidance(){
+		Debug.Log ("Updating Guidance");
+		guidanceVec = go.transform.position;
+		Invoke ("Unpause", timeToPause);
+		}
 
 	public void TurnAround(){
 		heading = heading*-1;
+		updateGuidance ();
 		paused = true;
-		Invoke ("Unpause", timeToPause);
 	}
 
 	void Unpause(){
