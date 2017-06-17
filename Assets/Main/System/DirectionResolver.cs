9f3b932aa13c48eafac654e78ff9d74cd71dbd75
Battlespud <battlespud@hotmail.com>
2017-06-17 08:12:10 -0400

    enjoi, courtesy- da boi

--- Assets/Main/System/DirectionResolver.cs
+++ Assets/Main/System/DirectionResolver.cs
@@ -4,51 +4,173 @@ using UnityEngine;
 
 public static class DirectionResolver {
 
+	static CameraDirection cd = Camera.main.GetComponent<CameraDirection> ();
+
 	public static Vector3 RayDirection(SpriteController sc){
 		Vector3 dirVec = new Vector3 ();
 		Facing facing = sc.finalFacing;
-		switch (facing) {
-		case(Facing.D):{
-				dirVec = Vector3.back;
-				break;
-			}
-		case(Facing.DL):{
-				dirVec = new Vector3 (-1, 0, -1);
-				break;
-			}
-		case(Facing.L):{
-				dirVec = Vector3.left;
-				break;
+		switch (cd.facing) {
 
-			}
-		case(Facing.UL):{
-				dirVec = new Vector3 (-1, 0, 1);
-				break;
+		case (Facing.D):
+			{
+				switch (facing) {
+				case(Facing.D):{
+						dirVec = Vector3.back;
+						break;
+					}
+				case(Facing.DL):{
+						dirVec = new Vector3 (-1, 0, -1);
+						break;
+					}
+				case(Facing.L):{
+						dirVec = Vector3.left;
+						break;
+					}
+				case(Facing.UL):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
+					}
+				case(Facing.U):{
+						dirVec = Vector3.forward;
+						break;
+					}
+				case(Facing.UR):{
+						dirVec = new Vector3 (1, 0, 1);
+						break;
+					}
+				case(Facing.R):{
+						dirVec = Vector3.right;
+						break;
+					}
+				case(Facing.DR):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
 
-			}
-		case(Facing.U):{
-				dirVec = Vector3.forward;
+					}
+				}
 				break;
-
 			}
-		case(Facing.UR):{
-				dirVec = new Vector3 (1, 0, 1);
-				break;
+		case (Facing.L):
+			{
+				switch (facing) {
+				case(Facing.D):{
+						dirVec = Vector3.back;
+						break;
+					}
+				case(Facing.DL):{
+						dirVec = new Vector3 (-1, 0, -1);
+						break;
+					}
+				case(Facing.L):{
+						dirVec = Vector3.left;
+						break;
+					}
+				case(Facing.UL):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
 
+					}
+				case(Facing.U):{
+						dirVec = Vector3.forward;
+						break;
+					}
+				case(Facing.UR):{
+						dirVec = new Vector3 (1, 0, 1);
+						break;
+					}
+				case(Facing.R):{
+						dirVec = Vector3.right;
+						break;
+					}
+				case(Facing.DR):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
+					}
+				}
+				dirVec *= -1;
+				break;
 			}
-		case(Facing.R):{
-				dirVec = Vector3.right;
+		case (Facing.U):
+			{
+				switch (facing) {
+				case(Facing.D):{
+						dirVec = Vector3.back;
+						break;
+					}
+				case(Facing.DL):{
+						dirVec = new Vector3 (-1, 0, -1);
+						break;
+					}
+				case(Facing.L):{
+						dirVec = Vector3.left;
+						break;
+					}
+				case(Facing.UL):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
+					}
+				case(Facing.U):{
+						dirVec = Vector3.forward;
+						break;
+					}
+				case(Facing.UR):{
+						dirVec = new Vector3 (1, 0, 1);
+						break;
+					}
+				case(Facing.R):{
+						dirVec = Vector3.right;
+						break;
+					}
+				case(Facing.DR):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
+					}
+				}
 				break;
-
 			}
-		case(Facing.DR):{
-				dirVec = new Vector3 (-1, 0, 1);
+		case (Facing.R):
+			{
+				switch (facing) {
+				case(Facing.D):{
+						dirVec = Vector3.back;
+						break;
+					}
+				case(Facing.DL):{
+						dirVec = new Vector3 (-1, 0, -1);
+						break;
+					}
+				case(Facing.L):{
+						dirVec = Vector3.left;
+						break;
+					}
+				case(Facing.UL):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
+					}
+				case(Facing.U):{
+						dirVec = Vector3.forward;
+						break;
+					}
+				case(Facing.UR):{
+						dirVec = new Vector3 (1, 0, 1);
+						break;
+					}
+				case(Facing.R):{
+						dirVec = Vector3.right;
+						break;
+					}
+				case(Facing.DR):{
+						dirVec = new Vector3 (-1, 0, 1);
+						break;
+					}
+				}
+				dirVec *= -1;
 				break;
-
 			}
 
 
 		}
+
 
 		return dirVec;
 
