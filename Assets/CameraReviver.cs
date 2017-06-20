57df30f44ffa32d4d6a29b71fdad6e66208e9104
Battlespud <battlespud@hotmail.com>
2017-06-20 19:29:04 -0400

    Added some camera stuff
    go to DebugScene_C or whatever and walk inside the buildings, new feature :D

---
+++ Assets/CameraReviver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraReviver : MonoBehaviour {
+
+	public Camera playerCam;
+
+
+	void Awake(){
+		playerCam = Camera.main;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (Camera.allCamerasCount == 0) {
+			if (!playerCam.gameObject.activeInHierarchy) {
+				playerCam.gameObject.SetActive (true);
+			}
+		}
+		else if (Camera.allCamerasCount > 1) {
+			if (playerCam.gameObject.activeInHierarchy) {
+				playerCam.gameObject.SetActive (false);
+			}
+	}
+}
+}
---
+++ Assets/Main/System/Environment/InteriorInMapTrigger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteriorInMapTrigger : MonoBehaviour {
+
+	[SerializeField]private Camera cam;
+	private Camera playerCam;
+
+	[SerializeField]GameObject wall;
+
+	Transform recordedTransform;
+
+	// Use this for initialization
+	void Start () {
+		playerCam = Camera.main;
+		cam.enabled = true;
+		cam.gameObject.SetActive (false);
+	}
+
+
+
+
+	void OnTriggerEnter(Collider col){
+		if (CheckIsPlayer(col.gameObject)) {
+			EntersInterior ();
+		}
+	}
+
+
+	void EntersInterior(){
+		//playerCam.gameObject.SetActive (false);
+	//	playerCam = camera.
+		cam.gameObject.SetActive(true);
+		wall.SetActive (false);
+		//makes it so the last known settings of the camera direction script is correct and stops the sprites + controls from glitching out if you change direction.
+		playerCam.gameObject.transform.SetPositionAndRotation (cam.gameObject.transform.position, cam.gameObject.transform.rotation);
+	}
+
+
+	void OnTriggerExit(Collider col){
+		if (CheckIsPlayer(col.gameObject)) {
+			ExitsInterior ();
+		}
+	}
+
+	void ExitsInterior(){
+	//	playerCam.gameObject.SetActive (true);
+		cam.gameObject.SetActive (false);
+		wall.SetActive (true);
+	}
+
+
+	bool CheckIsPlayer(GameObject obj){
+		if (obj.GetComponent<MovementController> () != null) {
+			if (obj.GetComponent<MovementController> ().isPlayer) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
